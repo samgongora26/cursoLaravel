636390c94c687d4c297596ae9ed49cab7e73e4e7
samgongora26 <saul.santiago2426@gmail.com>
2022-03-31 16:16:32 -0500

    agregando y eliminando usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -14,8 +14,39 @@
     </head>
     <body>
         <div class="container">
-            <div class="row">
+            <div class="row mt-3">
                 <div class="col-sm-8 mx-auto">
+
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            @if($errors->any())
+                            <div class="alert alert-danger">
+                                @foreach($errors->all() as $error)
+                                - {{ $error }} <br>
+                                @endforeach
+                            </div>
+                            @endif
+                            <h4>Agregar usuario</h4>
+                            <form action="{{ route('users.store') }}" method="POST">
+                                <div class="form-row">
+                                    <div class="col-sm-3">
+                                        <input type="text" name="name" class="form-control" placeholder="Nombre" value="{{ old('name')}}">
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <input type="text" name="email" class="form-control" placeholder="Email" value="{{ old('email')}}">
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <input type="password" name="password" class="form-control" placeholder="password" value="{{ old('password')}}">
+                                    </div>
+                                    <div class="col-auto">
+                                        @csrf
+                                        <button type="submit" class="btn btn-primary">Enviar</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <h4>Usuarios</h4>
                     <table class="table">
                         <thead>
                             <tr>
@@ -31,7 +62,15 @@
                                 <td>{{$user->id}}</td>
                                 <td>{{$user->name}}</td>
                                 <td>{{$user->email}}</td>
-                                <td>Eliminar</td>                                
+                                <td>
+                                    <form action="{{ route('users.destroy', $user )}}" method="POST">
+                                        @method('DELETE')
+                                        <!-- Esto le dice a Laravel que el formulario es nuestro. -->
+                                        @csrf
+                                        <input type="submit" value="Eliminar" class="btn btn-sm btn-danger"
+                                            onclick="return confirm('Desea eliminar?')">
+                                    </form>
+                                </td>                                
                             </tr>
                             @endforeach
                         </tbody>
--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/', [UserController::class, 'index']);
-Route::post('users', [UserController::class, 'store']);
-Route::delete('users/{user}', [UserController::class, 'destroy']);
+Route::post('users', [UserController::class, 'store'])->name('users.store');
+Route::delete('users/{user}', [UserController::class,'destroy'])->name('users.destroy');
